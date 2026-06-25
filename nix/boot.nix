e83348e5868a58c51c158ea5b4344bbce243e8de
mc0vafla <sssskrol@gmail.com>
2026-06-25 23:00:00 +0200

    1

--- nix/boot.nix
+++ nix/boot.nix
@@ -73,12 +73,6 @@
             linux /chimera/vmlinuz root=ZFS=rpool/ROOT zfs=rpool/ROOT rw quiet
             initrd /chimera/initrd.img
         }
-        menuentry "Debian" --class debian --class gnu-linux {
-            insmod btrfs
-            search --no-floppy --fs-uuid --set=root 8d7aecd6-b818-4a57-8e9e-46c3ba9f4434
-            linux /boot/vmlinuz-6.12.94+deb13-amd64 root=UUID=8d7aecd6-b818-4a57-8e9e-46c3ba9f4434 rw rootflags=subvol=@rootfs quiet
-            initrd /boot/initrd.img-6.12.94+deb13-amd64
-        }
       '';
     };
 
